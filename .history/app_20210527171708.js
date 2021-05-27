0a3558a67bee640054cc64a4fc4276b7af4c03d3
oksana117 <81168349+oksana117@users.noreply.github.com>
2021-05-27 18:00:12 -0400

    commit 4

--- .history/app_20210527171708.js
+++ .history/app_20210527171708.js
@@ -1,13 +1,13 @@
-let  createError = require('http-errors');
-let  express = require('express');
-let  path = require('path');
-let  cookieParser = require('cookie-parser');
-let  logger = require('morgan');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-let  indexRouter = require('./routes/index');
-let  usersRouter = require('./routes/users');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-let  app = express();
+var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
