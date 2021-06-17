ba70e24f025f24281fcc8cd1ee93168055c2a83d
oksana117 <81168349+oksana117@users.noreply.github.com>
2021-06-17 01:03:00 -0400

    Updated URI

---
+++ .history/server/config/db_20210617005512.js
@@ -0,0 +1,6 @@
+module.exports =
+{
+    "URI": "mongodb://localhost/business_contacts"
+    "URI": "mongodb+srv://oksana:<password>@mongodbserver.wuevp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";"
+    
+}
