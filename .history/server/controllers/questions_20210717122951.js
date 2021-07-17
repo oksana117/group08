7eeee51a59e25ec60e06f28c47a31990de15b9fe
oksana117 <81168349+oksana117@users.noreply.github.com>
2021-07-17 13:53:07 -0400

    error at create

---
+++ .history/server/controllers/questions_20210717122951.js
@@ -0,0 +1,138 @@
+  /* Team: Group 8
+  Web site name : Pandora-Survey
+  Date : July 18, 2021
+  Author's names & Student IDs:
+  Oksana Koshulap: 301167025
+  Remedios Meneses: 300691712
+  Anmary Gain: 301152014 
+  Tesmine Poulose: 301151876
+  Sapna: 301152192 */
+
+let express = require('express');
+  let router = express.Router();
+  let mongoose = require('mongoose');
+  
+  //let jwt = require('jsonwebtoken');
+  
+  //create a reference to the model
+let Questions = require('../models/questions');
+
+  module.exports.displayView= (req, res, next) => {
+    Questions.find((err, questionList) => {
+        if (err)
+        {
+            return console.error(err);
+        }
+        else
+        {
+            
+            res.render('survey/view', {
+                title: 'Questions List',
+                  QuestionsList: questionList,
+
+            displayName: req.user ? req.user.displayName : ''});      
+        }
+    });
+  }
+  
+module.exports.displayCreatePage = (req, res, next) =>{
+      res.render('survey/create', {title: 'Add New Survey',
+      displayName: req.user ? req.user.displayName : ''});
+  }
+  
+  module.exports.processCreatePage = (req, res, next) => {
+
+    // receive all the question text and the types here
+
+    // construct the newQuestions object
+
+    // loop that creates multiple questions
+
+    // start loop here
+    let newQuestions = Questions({
+        "question": req.body.question1,
+        //"type":req.body.question1,
+        "question": req.body.question2,
+        //"type": req.body.question2,
+        "question": req.body.question3,
+        "type": req.body.question3,
+        "question": req.body.question4,
+        "type": req.body.question4,
+        "question": req.body.question5,
+        "type": req.body.question5
+        //question type
+        //question/survey id
+    });
+
+    Questions.create(newQuestions , (err, question) =>{
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            //refresh the businesscontactlist
+            res.redirect('/survey-view');
+        }
+    });
+
+    // end loop here
+}
+  module.exports.displayUpdatePage = (req, res, next) =>{
+    let id = req.params.id;
+
+    Questions.findById(id, (err, surveyToEdit) => {
+        if(err) 
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            //show the edit view
+            res.render('survey/update', {title: 'Update Survey', survey: surveyToEdit})
+        }
+    });
+}
+  
+  module.exports.processUpdatePage= (req, res, next) =>{
+    let id = req.params.id
+
+    let updatedSurvey =Survey({
+        "_id": id,
+        "question": req.body.question
+    });
+
+    Questions.updateOne({_id: id}, updatedSurvey, (err) =>{
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            //refresh the businesscontactlist
+            res.redirect('/survey-view');
+
+        }
+    });
+}
+  
+  module.exports.performDelete = (req, res, next) =>{
+    let id = req.params.id;
+
+    Survey.remove({_id: id}, (err) =>{
+        if(err)
+       {
+           console.log(err);
+           res.end(err);
+       }
+       else
+       {
+            //refresh the businesscontactlist
+            res.redirect('/survey-view');
+       }
+    });
+  }
+  
---
+++ .history/server/models/survey_20210717131639.js
@@ -0,0 +1,33 @@
+/* Team: Group 8
+  Web site name : Pandora-Survey
+  Date : July 18, 2021
+  Author's names & Student IDs:
+  Oksana Koshulap: 301167025
+  Remedios Meneses: 300691712
+  Anmary Gain: 301152014 
+  Tesmine Poulose: 301151876
+  Sapna: 301152192 */
+
+let mongoose = require('mongoose');
+
+//create a model class
+let surveyModel = mongoose.Schema({
+  username: String,
+  displayName: String,
+  author: String,
+  questionsNumber: String, // need to change it to int!
+  question: String,
+  startDate: Date,
+  endDate: Date,
+  active: Boolean,
+  surveyID: String
+},
+{
+    collection: "survey"
+});
+
+
+
+module.exports = mongoose.model('Survey', surveyModel);
+
+
