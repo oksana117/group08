89853e54e94d3e7bd7b4f63530c7f50d943bbcc9
oksana117 <81168349+oksana117@users.noreply.github.com>
2021-07-27 15:25:13 -0400

    Auth

--- server/routes/index.js
+++ server/routes/index.js
@@ -32,6 +32,21 @@ router.get('/services', indexController.displayServicesPage);
 /* GET contact me page. */
 router.get('/contact', indexController.displayContactPage);
 
+/* GET Route for displaying the Login page */
+router.get('/login', indexController.displayLoginPage);
+
+/* POST Route for processing the Login page */
+router.post('/login', indexController.processLoginPage);
+
+/* GET Route for displaying the Register page */
+router.get('/register', indexController.displayRegisterPage);
+
+/* POST Route for processing the Register page */
+router.post('/register', indexController.processRegisterPage);
+
+/* GET to perform UserLogout */
+router.get('/logout', indexController.performLogout);
+
 
 
 
